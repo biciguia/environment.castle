4800c9331f607ffd1ead63ca8cc07de75641547f
albert philippe marcel de la fuente vigliotti <albert.vigliotti@hulk.cec.ime.usp.br>
2015-03-27 15:13:00 -0300

    pilot: @vonpupp, copilot: @ricardoteles. Added gitflow

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,15 @@
 # Install Git
 #sudo apt-get install git
 
+# Gitflow
+if [[ $1 == "gitflow" ]] ; then
+    mkdir ~/bin
+    wget -c https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh
+    chmod +x gitflow-installer.sh
+    INSTALL_PREFIX=~/bin ./gitflow-installer.sh
+    rm gitflow-installer.sh
+fi
+
 # Homesick
 git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
 source $HOME/.homesick/repos/homeshick/homeshick.sh
@@ -15,4 +24,3 @@ $LINKER -b clone https://github.com/biciguia/environment.castle.git
 if [[ $1 == "link" || $1 == "pull" ]] ; then
     $LINKER $1 environment.castle
 fi
-
