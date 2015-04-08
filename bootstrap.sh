5fa5859950dcc41259f420fe854c4f11487e4ca6
vonpupp <vonpupp@gmail.com>
2015-04-08 11:06:02 -0300

    pilot: @vonpupp, copilot: @fabiokaspar. Added git-prompt colors

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,17 +3,27 @@
 # Install Git
 #sudo apt-get install git
 
-# Gitflow
-if [[ $1 == "gitflow" ]] ; then
+if [[ $1 == "tools" ]] ; then
+    # Gitflow
     mkdir ~/bin
     wget -c https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh
     chmod +x gitflow-installer.sh
     INSTALL_PREFIX=~/bin ./gitflow-installer.sh
     rm gitflow-installer.sh
+    # Git-prompt
+    wget -O $HOME/bin/.git-prompt.sh -c https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh 
+    # Sublime
+    wget -O $HOME/labxp/subl.tar.bz2 -c http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
+    tar xvf $HOME/labxp/subl.tar.bz2 -C $HOME/labxp
+    ln -s $HOME/labxp/Sublime\ Text\ 2/sublime_text ~/bin/subl
+    rm $HOME/labxp/subl.tar.bz2
+    # Folders
+    mkdir ~/labxp
+    cd ~/labxp
+    git clone https://github.com/biciguia/biciguia-frontend.git
+    # Gems
+    gem install --user vagrant
 fi
-
-# Gems
-gem install --user vagrant
 
 # Homesick
 git clone https://github.com/andsens/homeshick.git $HOME/.homesick/repos/homeshick
@@ -27,18 +37,3 @@ $LINKER -b clone https://github.com/biciguia/environment.castle.git
 if [[ $1 == "link" || $1 == "pull" ]] ; then
     $LINKER $1 environment.castle
 fi
-
-# Folders
-mkdir ~/labxp
-cd ~/labxp
-git clone https://github.com/biciguia/biciguia-frontend.git
-
-# Sublime
-wget -O $HOME/labxp/subl.tar.bz2 -c http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
-tar xvf $HOME/labxp/subl.tar.bz2 -C $HOME/labxp
-ln -s $HOME/labxp/Sublime\ Text\ 2/sublime_text ~/bin/subl
-rm $HOME/labxp/subl.tar.bz2
-
-# Git-prompt
-wget -O $HOME/bin/.git-prompt.sh -c https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh 
-
--- home/.profile
+++ home/.profile
@@ -1,5 +1,4 @@
 PATH=$PATH:~/bin
 PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"
 source ~/bin/.git-prompt.sh
-export PS1="\[$GREEN\]\t\[$RED\]-\[$BLUE\]\u\[$YELLOW\]\[$YELLOW\]\w\[\033[m\]\[$MAGENTA\]\$(__git_ps1)\[$WHITE\]\$ "
-
+#export PS1="\[$GREEN\]\t\[$RED\]-\[$BLUE\]\u\[$YELLOW\]\[$YELLOW\]\w\[\033[m\]\[$MAGENTA\]\$(__git_ps1)\[$WHITE\]\$ "
