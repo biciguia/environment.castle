01b8a3f1d8b76bb4f059c83bfcf2adff081ad81e
vonpupp <vonpupp@gmail.com>
2015-05-27 11:05:30 -0300

    Added submodule initialization and update for local repo and for biciguia-frontend. [@vonpupp, @wwared]

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,27 +4,39 @@
 #sudo apt-get install git
 
 if [[ $1 == "tools" ]] ; then
+    # Update submodules
+    git submodule init
+    git submodule update
+
     # Gitflow
     mkdir ~/bin
     wget -c https://raw.github.com/nvie/gitflow/develop/contrib/gitflow-installer.sh
     chmod +x gitflow-installer.sh
     INSTALL_PREFIX=~/bin ./gitflow-installer.sh
     rm gitflow-installer.sh
+
     # Git-prompt
     wget -O $HOME/bin/.git-prompt.sh -c https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh 
+
     # Git-completion
     wget -O $HOME/bin/.git-completion.bash -c https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+
     # Sublime
     wget -O $HOME/labxp/subl.tar.bz2 -c http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64.tar.bz2
     tar xvf $HOME/labxp/subl.tar.bz2 -C $HOME/labxp
     ln -s $HOME/labxp/Sublime\ Text\ 2/sublime_text ~/bin/subl
     rm $HOME/labxp/subl.tar.bz2
+
     # Folders
     mkdir ~/labxp
     cd ~/labxp
     git clone https://github.com/biciguia/biciguia-frontend.git
+    cd biciguia-frontend
+    git submodule init
+    git submodule update
+
     # Gems
-    gem install --user vagrant
+    #gem install --user vagrant
 fi
 
 # Homesick
